c257a499a56c43009346689226a884a2e1973af4
elmar02 <babayefff02@gmail.com>
2025-05-11 08:34:38 +0400

    ui fixed

--- containers/stories/StoriesSection.tsx
+++ containers/stories/StoriesSection.tsx
@@ -17,51 +17,53 @@ const StoriesSection = ({ stories }: Props) => {
         search: ''
     })
 
-    const handleChange = (e: ChangeEvent<HTMLInputElement|HTMLSelectElement>)=>{
-        setFilter(prev=>({
+    const handleChange = (e: ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+        setFilter(prev => ({
             ...prev,
             [e.target.name]: e.target.value
         }))
     }
 
-    const filteredStories = useMemo(()=>{
-        const {genre, length, search} = filter
+    const filteredStories = useMemo(() => {
+        const { genre, length, search } = filter
         return filterStories(stories, length, genre, search)
-    },[filter])
+    }, [filter])
     return (
         <section id='stories' className='section-box section-sm space-y-3'>
-            <div className='flex items-center gap-3'>
+            <div className='grid sm:grid-cols-2 gap-2'>
                 <Input onChange={handleChange} value={filter.search} type='search' placeholder='Search' name='search' />
-                <Select onChange={handleChange} value={filter.genre} name='genre'>
-                    <Option value="">All Genres</Option>
-                    {
-                        genres.map((genre, index) => (
-                            <Option value={genre} key={index}>{genre}</Option>
-                        ))
-                    }
-                </Select>
-                <Select onChange={handleChange} value={filter.length} name='length'>
-                    <Option value="">All Lengths</Option>
-                    {
-                        lengths.map((length, index) => (
-                            <Option value={length} key={index}>{length}</Option>
-                        ))
-                    }
-                </Select>
+                <div className='grid grid-cols-2 gap-2'>
+                    <Select onChange={handleChange} value={filter.genre} name='genre'>
+                        <Option value="">All Genres</Option>
+                        {
+                            genres.map((genre, index) => (
+                                <Option value={genre} key={index}>{genre}</Option>
+                            ))
+                        }
+                    </Select>
+                    <Select onChange={handleChange} value={filter.length} name='length'>
+                        <Option value="">All Lengths</Option>
+                        {
+                            lengths.map((length, index) => (
+                                <Option value={length} key={index}>{length}</Option>
+                            ))
+                        }
+                    </Select>
+                </div>
             </div>
             {
                 stories.length !== 0 ?
-                    filteredStories.length === 0? <p className='text-center text-slate-500'>No matches found.</p>
-                    :<div className='grid gap-5 sm:grid-cols-2 lg:grid-cols-4'>
-                        {
-                            filteredStories.map((story, i) => (
-                                <StoryCard
-                                    key={i}
-                                    story={story}
-                                />
-                            ))
-                        }
-                    </div>
+                    filteredStories.length === 0 ? <p className='text-center text-slate-500'>No matches found.</p>
+                        : <div className='grid gap-5 sm:grid-cols-2 lg:grid-cols-4'>
+                            {
+                                filteredStories.map((story, i) => (
+                                    <StoryCard
+                                        key={i}
+                                        story={story}
+                                    />
+                                ))
+                            }
+                        </div>
                     : <p className='text-center text-slate-500'>There is no stories yet.</p>
             }
         </section>
